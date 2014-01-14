78aa94d7de552f75bd368c9e21473a0ff310951e
nimtex <basijzelendoorn@hotmail.com>
2014-01-14 15:59:40 +0100

    Update header.php
    about us - over ons

--- application/views/general/header.php
+++ application/views/general/header.php
@@ -23,7 +23,7 @@
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav">
       <li><a href="<?=base_url()?>">Home</a></li>
-      <li><a href="<?=site_url("proto/about");?>">About us</a></li>
+      <li><a href="<?=site_url("proto/about");?>">Over ons</a></li>
     </ul>
 
     <ul class="nav navbar-nav navbar-right">
