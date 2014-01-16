19d70b1e316b78b156d13a18650ac7f87be3f632
nimtex <basijzelendoorn@hotmail.com>
2014-01-16 18:12:22 +0100

    Update Almere.php
    tekst

--- application/views/Almere.php
+++ application/views/Almere.php
@@ -1,2 +1,29 @@
-<h1>Almere</h1>
-
+<div class="jumbotron">
+                <h1>Alles over Almere</h1>                        
+                <div class="row">
+                        <div class="col-md-4">
+                                <p>
+                                        <h3>Evenementen</h3>
+                                        <img src="http://imagene.youropi.com/intocht-sinterklaas-almere-2008-event-almer(p:event,6367)(c:0).jpg" height='250' width='250' class="img-responsive" />
+                                        <p>Kijk wanneer er evenementen zijn.</p>
+                                        <a class="btn btn-primary btn-lg" role="button">Ga naar evenemenenten</a>
+                                </p>
+                        </div>
+                        <div class="col-md-4">
+                                <p>
+                                        <h3>Geschiedenis</h3>
+                                        <img src="http://www.zuiderzeeland.nl/contents/pages/169622/eerstewoningenalmerehaven.jpg" height='250' width='250' class="img-responsive" />
+                                        <p>Lees hier over de geschiedenis van Almere.</p>
+                                        <a class="btn btn-primary btn-lg" role="button">Ga naar geschiedenis</a>
+                                </p>
+                        </div>
+                        <div class="col-md-4">
+                                <p>
+                                        <h3>Almere lokaal</h3>
+                                        <img src="http://www.boathouse.nl/images/boathouse.nl/cms/_galleries/SliderhomeWINTER/IMG_9105.jpg" height='250' width='250' class="img-responsive" />
+                                        <p>Vindt hier alle informatie over lokale lekernijen en cultuur.</p>
+                                        <a class="btn btn-primary btn-lg" role="button">Lees meer</a>
+                                </p>
+                        </div>
+                </div>
+        </div>
